396f8e09f2220dfe132d0bc78c576f31d0dc90d9
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2026-03-22 15:55:16 +0100

    revise

--- app.py
+++ app.py
@@ -19,7 +19,12 @@ def load_model():
 @st.cache_data
 def load_data():
     # Load data untuk dashboard
-    return pd.read_csv("data.csv", sep=";")
+    df = pd.read_csv("data.csv", sep=";")
+    
+    # TAMBAHKAN BARIS INI: Agar hasil Cloud sama dengan Lokal (Hanya Graduate & Dropout)
+    df = df[df['Status'] != 'Enrolled'] 
+    
+    return df
 
 model = load_model()
 df_raw = load_data()
